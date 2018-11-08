caa885bf7ee7ad93e36d13b74b211b2ddcc37d81
Barry Gitarts <bgitarts@gmail.com>
2018-11-08 17:47:26 -0500

    submit message on enter

--- app/components/ChatRoom.js
+++ app/components/ChatRoom.js
@@ -1,16 +1,13 @@
 // @flow
 import React, { Fragment } from 'react';
+import { Formik } from 'formik';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-import ChatBox from './ChatBox.js';
+import ChatBox from './ChatBox';
 
 
-const messageSend = (send, value) => {
-  send(value);
-  value = "";
-};
 const listStyle = { overflow: 'scroll', height: '85%' };
 const ChatRoom = ({ messages, sendMessage }) => (
   <div style={{ height: '100vh' }}>
@@ -32,15 +29,40 @@ const ChatRoom = ({ messages, sendMessage }) => (
         ))}
       </List>
       <Divider />
-        <TextField
-          id="outlined-search"
-          label="Type a message..."
-          type="text"
-          margin="normal"
-          variant="outlined"
-          fullWidth
-          onBlur={(e) => { messageSend(sendMessage, e.target.value) }}
-        />
+      <Formik
+        initialValues={{ chatInput: '' }}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+            const { chatInput } = values;
+            sendMessage(chatInput);
+            resetForm();
+            setSubmitting(false);
+        }}
+      >
+        {({
+           values,
+           errors,
+           touched,
+           handleChange,
+           handleBlur,
+           handleSubmit
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <TextField
+              id="chatInput"
+              label="Type a message..."
+              type="text"
+              name="chatInput"
+              margin="normal"
+              variant="outlined"
+              fullWidth
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.chatInput || ''}
+            />
+            {errors.chatInput && touched.chatInput && errors.chatInput}
+          </form>
+        )}
+      </Formik>
     </Grid>
   </div>
 );

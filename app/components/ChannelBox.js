7f353a0d34cf49ad4bd0f6dd79f08bc013971ddc
Barry Gitarts <bgitarts@gmail.com>
2018-11-09 18:22:02 -0500

    add chat context + room selection

--- app/components/ChannelBox.js
+++ app/components/ChannelBox.js
@@ -3,14 +3,19 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
+import { ChatContext } from '../context';
 
 const ChannelBox = ({ channelName, message }) => (
-  <ListItem>
-    <Avatar>
-      {channelName}
-    </Avatar>
-    <ListItemText primary={channelName} secondary={message} />
-  </ListItem>
+  <ChatContext.Consumer>
+    {({ setActiveChannel, currentChannel }) =>
+    <ListItem onClick={() => setActiveChannel(channelName)} selected={currentChannel == channelName}>
+      <Avatar>
+        {channelName}
+      </Avatar>
+      <ListItemText primary={channelName} secondary={message} />
+    </ListItem>
+    }
+  </ChatContext.Consumer>
 );
 
 export default ChannelBox;

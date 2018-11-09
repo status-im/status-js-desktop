216b2862dbaae43786562fa438b62fda530e8241
Barry Gitarts <bgitarts@gmail.com>
2018-11-09 18:29:11 -0500

    add light border seperator

--- app/components/ContextPanel.js
+++ app/components/ContextPanel.js
@@ -3,12 +3,14 @@ import Divider from '@material-ui/core/Divider';
 import ChannelList from './ChannelList';
 import ContextFilter from './ContextFilter';
 
+
+const borderStyle = { borderRight: '1px solid ghostwhite' };
 const ContextPanel = ({ channels, joinChannel }) => (
-  <Fragment>
+  <div style={borderStyle}>
     <ContextFilter joinChannel={joinChannel} />
     <Divider />
     <ChannelList channels={channels} />
-  </Fragment>
+  </div>
 );
 
 export default ContextPanel;

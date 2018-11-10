64dd9f19f5e38934107e9aa30f08ab3828bdf815
Barry Gitarts <bgitarts@gmail.com>
2018-11-10 07:30:20 -0500

    add channel title bar to channel

--- app/components/ChatRoom.js
+++ app/components/ChatRoom.js
@@ -1,15 +1,18 @@
 // @flow
 import React, { Fragment } from 'react';
 import { Formik } from 'formik';
+import Card from '@material-ui/core/Card';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import ChatBox from './ChatBox';
 
 
 const formStyle = { display: 'flex', justifyContent: 'center', alignItems: 'center' };
-const listStyle = { overflow: 'scroll', height: '88%' };
+const listStyle = { overflow: 'scroll', height: '78%' };
 const ChatRoom = ({ messages, sendMessage, currentChannel }) => (
   <div style={{ height: '100vh' }}>
     <Grid
@@ -19,6 +22,14 @@ const ChatRoom = ({ messages, sendMessage, currentChannel }) => (
       alignItems="stretch"
       style={{ height: '100%' }}
     >
+      <div>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {`#${currentChannel}`}
+          </Typography>
+        </CardContent>
+      </div>
+      <Divider />
       <List style={listStyle}>
         {messages[currentChannel] && messages[currentChannel].map((message, idx) => (
           <Fragment key={idx + message.message}>

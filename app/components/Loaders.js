4ea5ec07654621bfdde24d9308faf70909b52749
Iuri Matias <iuri.matias@gmail.com>
2018-11-24 09:21:52 -0500

    update loading animation

--- app/components/Loaders.js
+++ app/components/Loaders.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { css } from 'react-emotion';
-import { RingLoader, HashLoader, GridLoader } from 'react-spinners';
+import { BounceLoader, GridLoader } from 'react-spinners';
 
 const containerStyle = { display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', margin: '0 30% 0 30%' };
 export const FullScreenLoader = () => (
   <div className='sweet-loading' style={containerStyle}>
-    <HashLoader
+    <BounceLoader
       sizeUnit={"px"}
       size={150}
       color={'#4A90E2'}
@@ -22,5 +22,4 @@ export const FullScreenGridLoader = () => (
     />
   </div>
 )
-
 
